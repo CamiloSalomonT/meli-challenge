2f5b34bb769c673bd6b52169415ce3ba3c5e0e76
Camilo Salomon <camilo.salomont@gmail.com>
2021-04-03 01:09:03 -0500

    Better performance with this numbers. Added the enconding default parameter.

--- tecnico/flask/test.py
+++ tecnico/flask/test.py
@@ -11,9 +11,9 @@ def time_consumption(test):
     print("Time consumption for {}: {:.3f}s".format(test, time.time() - t0))
 
 
-with time_consumption("mini"):
-    Pipeline.call_pipeline("sample_data/tech_sample.csv")
+# with time_consumption("mini"):
+#     Pipeline.call_pipeline("sample_data/tech_sample.csv")
 
 
 with time_consumption("sample"):
-    Pipeline.call_pipeline("sample_data/technical_challenge_data.csv")
+    Pipeline.call_pipeline("/sample_data/technical_challenge_data.csv")
